9ef208048a7b70cd6851451479dd322bb722f90c
Sandra Ptaszek <staszek@desmart.com>
2021-05-04 13:34:08 +0200

    fix: change line height of exam header

--- src/components/Exam/Header/styles.ts
+++ src/components/Exam/Header/styles.ts
@@ -43,7 +43,7 @@ export const TimeAndPageContainer = styled.div`
 
 export const ExamHeaderTitle = styled.h1`
   color: ${props => props.theme.palette.white};
-  line-height: 40px;
+  line-height: 45px;
   font-weight: 400 !important;
   font-size: 21.33px;
   font-family: ${props => props.theme.typography.fontFamilySecondary};
