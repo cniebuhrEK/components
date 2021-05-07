81381782cb57b230c7b28fb68aed295dca930ef2
Sandra Ptaszek <staszek@desmart.com>
2021-05-07 12:32:08 +0200

    feat: end section modal

---
+++ example/src/stories/Exam/Modal/variants/EndSectionModal.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { EndSectionModal } from './EndSectionModal';
+
+export default {
+  title: 'Exam/Modal',
+  component: EndSectionModal
+};
+
+const Template = (args) => <EndSectionModal {...args} />;
+
+export const EndSection = Template.bind({});
+EndSection.args = {
+  incomplete: 12
+};
