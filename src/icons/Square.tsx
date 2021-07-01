3243e8e2800455df57e64597269a71190e945db8
Sandra Ptaszek <staszek@desmart.com>
2021-07-01 12:42:40 +0200

    feat: add drawer icons

---
+++ src/icons/Square.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Icon from '../components/Icon'
+
+export const Square = props => {
+  return (
+    <Icon
+      width='1em'
+      height='1em'
+      viewBox='0 0 506.1 506.1'
+      version='1.1'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <g>
+        <path d='m489.609 506.1h-473.118c-9.108 0-16.491-7.384-16.491-16.491v-473.118c0-9.108 7.383-16.491 16.491-16.491h473.119c9.107 0 16.49 7.383 16.49 16.491v473.118c0 9.107-7.383 16.491-16.491 16.491zm-456.628-32.982h440.138v-440.137h-440.138z' />
+      </g>
+    </Icon>
+  )
+}
+
+export default Square
