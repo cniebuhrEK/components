dec1afb9553f6aaaee5f3f1b9963e50c7e45f7e4
Sandra Ptaszek <staszek@desmart.com>
2021-09-28 13:22:31 +0200

    fix: add missing icon button colors

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -12,6 +12,7 @@ interface ButtonProps {
   id?: string
   name?: string
   variant?: 'filled' | 'transparent'
+  color?: 'orange' | 'green' | 'blue' | 'transparent'
 }
 
 const variants = {
@@ -19,8 +20,25 @@ const variants = {
   transparent: 'transparent'
 }
 
+const buttonColors = {
+  orange: 'orange',
+  green: 'green',
+  blue: 'blue',
+  transparent: 'transparent'
+}
+
 const IconButton = (props: ButtonProps): JSX.Element => {
-  const { onClick, icon, type, disabled, autofocus, id, name, variant } = props
+  const {
+    onClick,
+    icon,
+    type,
+    disabled,
+    autofocus,
+    id,
+    name,
+    variant,
+    color = buttonColors.orange
+  } = props
 
   return (
     <StyledButton
@@ -31,6 +49,7 @@ const IconButton = (props: ButtonProps): JSX.Element => {
       type={type}
       id={id}
       variant={variant}
+      color={color}
       {...props}
     >
       {icon}
@@ -52,20 +71,42 @@ export const StyledButton = styled.button`
   width: 32px;
   align-items: center;
   justify-content: center;
-  color: ${({ theme, variant }) => {
+  color: ${({ variant, color, theme }) => {
     switch (true) {
-      case variant === variants.filled:
+      case variant === variants.filled && color === buttonColors.orange:
         return theme.palette.darkblue01
+      case variant === variants.filled && color === buttonColors.green:
+        return theme.palette.biege
+      case variant === variants.filled && color === buttonColors.blue:
+        return theme.palette.orange01
+      case variant === variants.filled && color === buttonColors.transparent:
+        return theme.palette.grey07
+
+      case variant === variants.transparent && color === buttonColors.orange:
+        return theme.palette.orange02
+      case variant === variants.transparent && color === buttonColors.green:
+        return theme.palette.green04
+      case variant === variants.transparent && color === buttonColors.blue:
+        return theme.palette.darkblue01
+      case variant === variants.transparent &&
+        color === buttonColors.transparent:
+        return theme.palette.grey07
       default:
         return theme.palette.darkblue01
     }
   }};
-  background-color: ${({ theme, variant }) => {
+  background-color: ${({ variant, color, theme }) => {
     switch (true) {
-      case variant === variants.filled:
+      case variant === variants.filled && color === buttonColors.orange:
         return theme.palette.orange02
-      default:
+      case variant === variants.filled && color === buttonColors.green:
+        return theme.palette.green04
+      case variant === variants.filled && color === buttonColors.blue:
+        return theme.palette.darkblue01
+      case variant === variants.filled && color === buttonColors.transparent:
         return 'none'
+      default:
+        return theme.palette.orange02
     }
   }};
   box-shadow: none;
@@ -73,10 +114,26 @@ export const StyledButton = styled.button`
   transition: all 300ms ${({ theme }) => theme.transitions.easing.easeInOut} 0ms;
 
   svg {
-    color: ${({ theme, variant }) => {
+    color: ${({ theme, variant, color }) => {
       switch (true) {
-        case variant === variants.filled:
+        case variant === variants.filled && color === buttonColors.orange:
           return theme.palette.darkblue01
+        case variant === variants.filled && color === buttonColors.green:
+          return theme.palette.biege
+        case variant === variants.filled && color === buttonColors.blue:
+          return theme.palette.orange01
+        case variant === variants.filled && color === buttonColors.transparent:
+          return theme.palette.grey07
+
+        case variant === variants.transparent && color === buttonColors.orange:
+          return theme.palette.orange02
+        case variant === variants.transparent && color === buttonColors.green:
+          return theme.palette.green04
+        case variant === variants.transparent && color === buttonColors.blue:
+          return theme.palette.darkblue01
+        case variant === variants.transparent &&
+          color === buttonColors.transparent:
+          return theme.palette.grey07
         default:
           return theme.palette.darkblue01
       }
@@ -91,12 +148,32 @@ export const StyledButton = styled.button`
 
   &:hover:enabled,
   &:active:enabled {
-    background-color: ${({ theme, variant }) => {
+    color: ${({ color, theme }) => {
       switch (true) {
-        case variant === variants.filled:
+        case color === buttonColors.orange:
+          return theme.palette.background
+        case color === buttonColors.blue:
+          return theme.palette.background
+        case color === buttonColors.green:
+          return theme.palette.biege
+        case color === buttonColors.transparent:
           return theme.palette.orange01
         default:
-          return theme.palette.gray10
+          return theme.palette.background
+      }
+    }};
+    background-color: ${({ color, theme }) => {
+      switch (true) {
+        case color === buttonColors.orange:
+          return theme.palette.orange01
+        case color === buttonColors.blue:
+          return theme.palette.darkblue04
+        case color === buttonColors.green:
+          return theme.palette.green04
+        case color === buttonColors.transparent:
+          return theme.palette.grey08
+        default:
+          return theme.palette.orange01
       }
     }};
   }
@@ -105,7 +182,8 @@ export const StyledButton = styled.button`
 IconButton.defaultProps = {
   disabled: false,
   type: 'button',
-  variant: 'transparent'
+  variant: 'transparent',
+  color: 'orange'
 }
 
 export default IconButton
