0ac45be73599f7cc6026c6771afeb4356518a6a8
Sandra Ptaszek <staszek@desmart.com>
2021-09-29 10:38:45 +0200

    fix: refactor animation player to what is on the designs

--- src/components/Animation/Animation.stories.js
+++ src/components/Animation/Animation.stories.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Animation from './Animation'
-import data from './heartbeat.json'
+import example from './example.json'
 
 const Template = args => <Animation {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  data: data,
-  isPlaying: true,
-  autoplay: true,
-  loop: true,
-  isPaused: false,
-  width: 300,
-  height: 300,
-  speed: 1
+  data: example
 }
 
 export default {
--- src/components/Animation/Animation.tsx
+++ src/components/Animation/Animation.tsx
@@ -1,45 +1,90 @@
 import React from 'react'
 import Lottie from 'react-lottie'
+import styled from 'styled-components'
 
-// NOTE: Playing and pausing the component can be controlled manually by clicking
-// the animation. This overrides the component props. Controlling the animation
-// programmatically could possibly be disrupted by the user.
-type AnimationProps = {
-  loop?: boolean
-  autoplay?: boolean
-  width: number
-  height: number
-  isPlaying?: boolean
-  isPaused?: boolean
+import PlayIcon from '../../icons/Play'
+import PauseIcon from '../../icons/Pause'
+import StopIcon from '../../icons/Stop'
 
-  // FIXME: Speed doesn't seem to be respected by React Lottie.
-  speed?: number
+interface AnimationProps {
   data: any
 }
 
 const Animation = (props: AnimationProps): JSX.Element => {
-  const { loop, autoplay, speed, data, width, height, isPlaying, isPaused } =
-    props
+  const { data } = props
+  const dropdownRef = React.useRef(null)
 
-  const options = {
-    loop,
-    autoplay: autoplay && !isPaused,
-    animationData: data,
-    speed,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
+  const [isSpeedOpen, setIsSpeedOpen] = React.useState(false)
+  const [isPaused, setIsPaused] = React.useState(false)
+  const [isStopped, setIsStopped] = React.useState(true)
+  const [speed, setSpeed] = React.useState(1)
+
+  const handleStop = () => setIsStopped(true)
+  const handlePlay = () => setIsStopped(false)
+  const handlePause = () => setIsPaused(!isPaused)
+  const openSpeedMenu = () => setIsSpeedOpen(true)
+  const handleSelectSpeed = speed => () => {
+    setSpeed(speed)
+    setIsSpeedOpen(false)
   }
 
+  const options = {
+    animationData: data,
+    autoplay: false
+  }
+
+  React.useEffect(() => {
+    function handleClickOutside(event) {
+      // @ts-ignore
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsSpeedOpen(false)
+      }
+    }
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [dropdownRef])
+
   return (
-    <Lottie
-      options={options}
-      isPlaying={isPlaying && !isPaused}
-      isPaused={isPaused}
-      width={width}
-      height={height}
-      speed={speed}
-    />
+    <AnimationContainer>
+      <Lottie
+        options={options}
+        isStopped={isStopped}
+        isPaused={isPaused}
+        width={208}
+        speed={speed}
+      />
+      <div className='controls-container'>
+        <SpeedSelection isOpen={isSpeedOpen} ref={dropdownRef}>
+          <div className='selected-speed' onClick={openSpeedMenu}>
+            {speed} x
+          </div>
+          <div className='speed-options'>
+            <div className='speed-option' onClick={handleSelectSpeed(1)}>
+              1 x
+            </div>
+            <div className='speed-option' onClick={handleSelectSpeed(1.5)}>
+              1.5 x
+            </div>
+            <div className='speed-option' onClick={handleSelectSpeed(2)}>
+              2 x
+            </div>
+          </div>
+        </SpeedSelection>
+        <button className='animation-control stop' onClick={handleStop}>
+          <StopIcon />
+        </button>
+        <button className='animation-control play' onClick={handlePlay}>
+          <PlayIcon />
+        </button>
+        <button className='animation-control pause' onClick={handlePause}>
+          <PauseIcon />
+        </button>
+      </div>
+    </AnimationContainer>
   )
 }
 
@@ -51,3 +96,67 @@ Animation.defaultProps = {
 }
 
 export default Animation
+
+export const AnimationContainer = styled.div`
+  display: inline-block;
+
+  .controls-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .animation-control {
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    font-size: 9px;
+    color: ${({ theme }) => theme.palette.darkblue02};
+
+    &:not(last-of-type) {
+      margin-right: 9px;
+    }
+  }
+
+  .play {
+    color: ${({ theme }) => theme.palette.orange01};
+  }
+`
+
+const SpeedSelection = styled.div`
+  color: ${({ theme }) => theme.palette.darkblue02};
+  font-size: 11px;
+  line-height: 15px;
+  letter-spacing: -0.1px;
+  border: 1px solid ${({ theme }) => theme.palette.darkblue02};
+  border-radius: ${({ theme }) => theme.shape.borderRadiusNormal};
+  position: relative;
+  min-width: 30px;
+  text-align: center;
+  margin-right: 9px;
+  cursor: pointer;
+
+  //.selected-speed {
+  //
+  //}
+
+  .speed-options {
+    opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+    position: absolute;
+    background-color: ${({ theme }) => theme.palette.biege};
+    box-shadow: ${props => props.theme.shadows.darkShadow};
+    width: 100%;
+    top: calc(100% + 5px);
+    left: 0;
+    transition: opacity 300ms
+      ${({ theme }) => theme.transitions.easing.easeInOut};
+  }
+
+  .speed-option {
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.palette.grey10};
+    }
+  }
+`
