027f63da6c5db8575f6ef5f01369de8d5fb83ce0
Marcin Polonis <polo@desmart.com>
2022-02-18 14:55:05 +0100

    fix: remove CorrectAnswer and Incorrect svg icons; use icon components in RadioButton instead of url

---
+++ src/icons/CorrectMark.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Icon from '../components/Icon'
+
+export const CorrectMark = props => {
+  return (
+    <Icon width='20' height='20' viewBox='0 0 20 16' fill='none' {...props}>
+      <g>
+        <path
+          d='M7.861,15.665c-0.331,0-0.649-0.131-0.884-0.366L0.561,8.883c-0.488-0.489-0.488-1.28,0-1.768
+		s1.279-0.488,1.768,0l5.449,5.449l9.819-11.782c0.441-0.53,1.23-0.601,1.76-0.16c0.531,0.442,0.603,1.23,0.16,1.761L8.821,15.215
+		c-0.225,0.271-0.553,0.433-0.904,0.449C7.898,15.665,7.879,15.665,7.861,15.665z'
+        />
+      </g>
+    </Icon>
+  )
+}
+
+export default CorrectMark
