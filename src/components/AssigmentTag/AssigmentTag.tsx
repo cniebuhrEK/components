2b69141c610e51a394f077627a7f59f3e94eabfb
Sandra Ptaszek <staszek@desmart.com>
2022-02-28 13:38:59 +0100

    fix: correct AssigmentTag dimensions to be the same as on figma designs

--- src/components/AssigmentTag/AssigmentTag.tsx
+++ src/components/AssigmentTag/AssigmentTag.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import styled from 'styled-components'
-import CloseIcon from '../../icons/Close'
+import CloseContainedIcon from '../../icons/CloseContained'
 
 interface AssigmentTagProps {
   children?: JSX.Element | string
@@ -16,7 +16,7 @@ const AssigmentTag = (props: AssigmentTagProps): JSX.Element => {
     <AssigmentTagContainer {...props}>
       {children}
       <Delete onClick={onDelete}>
-        <CloseIcon />
+        <CloseContainedIcon />
       </Delete>
     </AssigmentTagContainer>
   )
@@ -29,28 +29,26 @@ AssigmentTag.defaultProps = {
 const AssigmentTagContainer = styled.div`
   display: inline-flex;
   align-items: center;
+  justify-content: center;
   background: ${({ theme }) => theme.palette.grey12};
-  padding: 4px 12px;
+  padding: 0 12px;
   border-radius: 20px;
-  line-height: 20px;
+  line-height: 24px;
+  font-size: 14px;
+  letter-spacing: -0.1px;
 `
 const Delete = styled.div`
-  display: flex;
-  justify-content: center;
+  display: inline-flex;
   align-items: center;
-  width: 18px;
-  height: 18px;
-  color: ${({ theme }) => theme.palette.white};
-  background: ${({ theme }) => theme.palette.grey13};
-  border-radius: 10px;
-  padding: 3px;
-  margin-left: 10px;
+  justify-content: center;
+  font-size: 13px;
+  color: ${({ theme }) => theme.palette.grey13};
+  margin-left: 9px;
   cursor: pointer;
   transition: all 800ms ${({ theme }) => theme.transitions.easing.easeInOut} 0ms;
 
   &:hover {
-    color: ${({ theme }) => theme.palette.grey13};
-    background: ${({ theme }) => theme.palette.white};
+    color: ${({ theme }) => theme.palette.white};
   }
 `
 
