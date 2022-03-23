350b9c4a111b6d65bebdfd76d1ed893271948281
Sandra Ptaszek <staszek@desmart.com>
2022-03-23 16:21:37 +0100

    test: added light and dark theme compare test and added it as a precommit

--- src/__tests__/basic.test.js
+++ src/__tests__/basic.test.js
@@ -1,3 +1,44 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
+import lightTheme from '../theme/themeLight'
+import darkTheme from '../theme/themeDark'
+
+export const generateArrayOfAllAvailablePaths = (
+  obj,
+  acc = [],
+  path = [],
+  level = 0
+) => {
+  Object.keys(obj).forEach((key, i) => {
+    if (level === 0) {
+      path = []
+    }
+
+    if (path.length > level) {
+      path.pop()
+    }
+
+    path.push(key)
+
+    if (typeof obj[key] === 'string') {
+      acc.push(path.join('.'))
+
+      path.pop()
+
+      if (i === Object.keys(obj).length - 1) {
+        path.pop()
+      }
+
+      return acc
+    } else {
+      return generateArrayOfAllAvailablePaths(obj[key], acc, path, level + 1)
+    }
+  })
+
+  return acc
+}
+
+test('Checks if darkTheme and lightTheme have the same paths', () => {
+  const lightThemePaths = generateArrayOfAllAvailablePaths(lightTheme)
+  const darkThemePaths = generateArrayOfAllAvailablePaths(darkTheme)
+
+  expect(lightThemePaths).toEqual(darkThemePaths)
 })
