e179d0fe65d599b87234045a9e01b8d968f466e9
Sandra Ptaszek <staszek@desmart.com>
2022-03-31 12:47:22 +0200

    fix: correct icon viewbox

--- src/icons/OpenBookIcon.tsx
+++ src/icons/OpenBookIcon.tsx
@@ -6,7 +6,7 @@ export const OpenBookIcon = props => {
     <Icon
       width='1em'
       height='1em'
-      viewBox='0 0 16 16'
+      viewBox='0 0 1170 1024'
       version='1.1'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
