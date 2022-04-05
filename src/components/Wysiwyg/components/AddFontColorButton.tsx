cc84a70123ea0868d53b1dd5ebf7d8210d817959
Sandra Ptaszek <staszek@desmart.com>
2022-04-05 07:55:24 +0200

    fix: lint

--- src/components/Wysiwyg/components/AddFontColorButton.tsx
+++ src/components/Wysiwyg/components/AddFontColorButton.tsx
@@ -165,7 +165,6 @@ const AddAdminHighlightsButton = (
             className='picker-color color-lightBrown'
           />
           {/* plop_create_font_color_blot_component */}
-
         </div>
       </div>
     </ButtonContainer>
