aa119aec6e5b7db2eea9a2bda7b489a0cad43db2
Sandra Ptaszek <staszek@desmart.com>
2022-04-14 09:38:52 +0200

    feat: move bouncing loader to components lib

---
+++ src/components/BouncingLoader/BouncingLoader.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+const BouncingLoader = styled.div`
+  margin-right: 20px;
+  color: ${({ theme }) => theme.colors.main.primary200};
+  position: relative;
+  display: inline-block;
+  height: 40px;
+  width: 15px;
+
+  &:before {
+    position: absolute;
+    content: '';
+    display: block;
+    top: 0;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: #fbae17;
+    transform-origin: 50%;
+    animation: bounce 500ms alternate infinite ease;
+  }
+
+  @keyframes bounce {
+    0% {
+      top: 30px;
+      height: 5px;
+      border-radius: 60px 60px 20px 20px;
+      transform: scaleX(2);
+    }
+    35% {
+      height: 15px;
+      border-radius: 50%;
+      transform: scaleX(1);
+    }
+    100% {
+      top: 0;
+    }
+  }
+`
+export default BouncingLoader
---
+++ src/components/BouncingLoader/index.ts
@@ -0,0 +1,1 @@
+export { default as BouncingLoader } from './BouncingLoader'
