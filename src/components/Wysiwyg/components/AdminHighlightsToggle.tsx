647a035b9999f2189321d1e12acfdff7f568d2df
Sandra Ptaszek <staszek@desmart.com>
2022-07-07 11:47:11 +0200

    fix: add toggle in wysiwyg editor for showing admin highlights

---
+++ src/components/Wysiwyg/components/AdminHighlightsToggle.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ToggleSwitch } from '../../index'
+import styled from 'styled-components'
+
+export const AdminHighlightsToggle = ({
+  toggleAdminHighlights
+}): JSX.Element => {
+  const handleToggle = () => toggleAdminHighlights()
+
+  return (
+    <ToggleContainer>
+      <ToggleSwitch
+        id='switch-admin-highlights'
+        name='switch-admin-highlights'
+        onChange={handleToggle}
+        label='Show highlights'
+      />
+    </ToggleContainer>
+  )
+}
+
+export default AdminHighlightsToggle
+
+const ToggleContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  bottom: 7px;
+
+  * {
+    font-size: 11px;
+  }
+`
