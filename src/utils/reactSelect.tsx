40d9fbbced2bf7278f6935bbe1310a3dc65a0f3e
Piotr Gorczyca <p.gorczyca@desmart.com>
2022-07-08 12:59:15 +0200

    fix:changed max-width of single select content and added checkmark color

--- src/utils/reactSelect.tsx
+++ src/utils/reactSelect.tsx
@@ -225,7 +225,7 @@ export const REACT_SELECT_STYLES = {
       },
       fontSize: '14px',
       wordBreak: 'keep-all',
-      maxWidth: '60px',
+      maxWidth: '80px',
       whiteSpace: 'nowrap',
       textOverflow: 'ellipsis'
     }
