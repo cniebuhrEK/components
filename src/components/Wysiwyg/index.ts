1d576f8d295bbaf6bf577907376f3d9ee485fe82
Sandra Ptaszek <staszek@desmart.com>
2022-12-02 14:46:03 +0100

    feat: create plain html renderer with quill classes

--- src/components/Wysiwyg/index.ts
+++ src/components/Wysiwyg/index.ts
@@ -3,4 +3,5 @@
 export { default as WysiwygEditor } from './WysiwygEditor'
 export { default as WysiwygViewer } from './WysiwygViewer'
 export { default as QuillHtmlConverter } from './QuillHtmlConverter'
+export { default as HtmlRenderer } from './HtmlRenderer'
 export { HIGHLIGHT_BLOTS } from './utils'
