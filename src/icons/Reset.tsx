8e2c1fafacbf4156f710eccc0a05f488fe229838
Marcin Polonis <polo@desmart.com>
2024-05-14 15:36:10 +0200

    feat: add icons for calendar

--- src/icons/Reset.tsx
+++ src/icons/Reset.tsx
@@ -11,10 +11,11 @@ export const Reset = props => {
       xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
-      <g>
+      <g id='CalendarReorderResetButtons_Reset'>
         <path
           id='Vector'
           d='M14.996 6.90039C15.0701 7.29789 15.1105 7.70886 15.1105 8.12825C15.1105 11.7781 12.1512 14.7375 8.50134 14.7375C6.44481 14.7375 4.60723 13.7976 3.39453 12.3238'
+          stroke='white'
           strokeWidth='1.6843'
           strokeLinecap='round'
           strokeLinejoin='round'
@@ -22,6 +23,7 @@ export const Reset = props => {
         <path
           id='Vector_2'
           d='M1.94284 8.95546C1.90915 8.68429 1.89062 8.40807 1.89062 8.12679C1.89062 4.4769 4.84995 1.51758 8.49983 1.51758C10.4688 1.51758 12.2356 2.37826 13.4466 3.74254'
+          stroke='white'
           strokeWidth='1.6843'
           strokeLinecap='round'
           strokeLinejoin='round'
@@ -29,6 +31,7 @@ export const Reset = props => {
         <path
           id='Vector_3'
           d='M13.4076 0.871094L13.6265 3.98369L10.5156 3.97863'
+          stroke='white'
           strokeWidth='1.6843'
           strokeLinecap='round'
           strokeLinejoin='round'
@@ -36,21 +39,12 @@ export const Reset = props => {
         <path
           id='Vector_4'
           d='M3.40478 15.1302L3.1875 12.0176L6.29841 12.0226'
+          stroke='white'
           strokeWidth='1.6843'
           strokeLinecap='round'
           strokeLinejoin='round'
         />
       </g>
-      <defs>
-        <clipPath id='clip0_15073_276442'>
-          <rect
-            width='16.0009'
-            height='16.0009'
-            fill='white'
-            transform='translate(0.5)'
-          />
-        </clipPath>
-      </defs>
     </Icon>
   )
 }
--- src/icons/WarningCalendar.tsx
+++ src/icons/WarningCalendar.tsx
@@ -11,7 +11,7 @@ export const WarningCalendar = props => {
       xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
-      <g>
+      <g id='warning-1_svgrepo.com' clipPath='url(#clip0_15082_221913)'>
         <path
           id='Vector'
           d='M20.475 31.1941C21.5528 31.1941 22.4265 30.3203 22.4265 29.2425C22.4265 28.1647 21.5528 27.291 20.475 27.291C19.3972 27.291 18.5234 28.1647 18.5234 29.2425C18.5234 30.3203 19.3972 31.1941 20.475 31.1941Z'
@@ -20,6 +20,7 @@ export const WarningCalendar = props => {
         <path
           id='Vector_2'
           d='M20.4805 15.584V23.3901'
+          stroke='white'
           strokeWidth='3.90306'
           strokeLinecap='round'
           strokeLinejoin='round'
@@ -27,6 +28,7 @@ export const WarningCalendar = props => {
         <path
           id='Vector_3'
           d='M3.7921 31.3994L16.992 4.99935C18.4305 2.1226 22.5357 2.12262 23.9742 4.99935L37.1742 31.3994C38.4717 33.9946 36.5846 37.048 33.6831 37.048H7.2831C4.38162 37.048 2.49451 33.9946 3.7921 31.3994Z'
+          stroke='white'
           strokeWidth='3.90306'
           strokeLinecap='round'
           strokeLinejoin='round'
