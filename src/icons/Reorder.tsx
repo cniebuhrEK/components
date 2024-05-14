0c6b3709b0735794dc4e922266259f5d5f92d22d
Marcin Polonis <polo@desmart.com>
2024-05-14 14:39:53 +0200

    feat: add icons for calendar

--- src/icons/Reorder.tsx
+++ src/icons/Reorder.tsx
@@ -15,10 +15,18 @@ export const Reorder = props => {
         <path
           id='Vector'
           d='M1.38281 1.63477H13.5906C14.5002 1.63477 15.2362 2.37081 15.2362 3.28033V5.5053C15.2362 6.41482 14.5002 7.15086 13.5906 7.15086H3.02838C2.11885 7.15086 1.38281 7.8869 1.38281 8.79643V11.0214C1.38281 11.9309 2.11885 12.667 3.02838 12.667H14.3722'
+          stroke='white'
+          strokeWidth='1.6843'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           id='Vector_2'
           d='M13.2109 10.416L15.6195 12.6679L13.2109 14.9013'
+          stroke='white'
+          strokeWidth='1.6843'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </g>
     </Icon>
